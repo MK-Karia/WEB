5bf3d7e72d04a55f52c928c30bee99f676ad01fa
MK-Karia <mk.karia5@gmail.com>
2024-03-27 11:54:40 +0300

    for lab4

--- api.php
+++ api.php
@@ -3,7 +3,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 echo $method;
 
 if ($method != "POST") {
-    echo "Ожидался другой метод";
+  echo "Ожидался другой метод";
 } else {
     $dataAsJson = file_get_contents("php://input");
     $dataAsArray = json_decode($dataAsJson, true);
@@ -11,25 +11,24 @@ if ($method != "POST") {
 }
 
 function saveImage(string $imageBase64) {
-  $imageBase64Array = explode(';base64,', $imageBase64);
-  $imgExtention = str_replace('data:image/', '', $imageBase64Array[0]);
-  $imageDecoded = base64_decode($imageBase64Array[1]);
-  saveFile("images/my-image.{$imgExtention}", $imageDecoded);
+    $imageBase64Array = explode(';base64,', $imageBase64);
+    $imgExtention = str_replace('data:image/', '', $imageBase64Array[0]);
+    $imageDecoded = base64_decode($imageBase64Array[1]);
+    saveFile("images/my-image.{$imgExtention}", $imageDecoded);
 }
 
 function saveFile(string $file, string $data): void {
     $myFile = fopen($file, 'w');
     if ($myFile) {
-      $result = fwrite($myFile, $data);
-      if ($result) {
-        echo 'Данные успешно сохранены в файл';
-      } else {
-        echo 'Произошла ошибка при сохранении данных в файл';
-      }
-      fclose($myFile);
+        $result = fwrite($myFile, $data);
+        if ($result) {
+            echo 'Данные успешно сохранены в файл';
+        } else {
+            echo 'Произошла ошибка при сохранении данных в файл';
+        }
+            fclose($myFile);
     } else {
-      echo 'Произошла ошибка при открытии файла';
+        echo 'Произошла ошибка при открытии файла';
     }
   }
-
 ?>
